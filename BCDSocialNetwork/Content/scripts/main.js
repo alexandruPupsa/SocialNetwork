fcbcf54dc2d7991c6190723ad89b20683ff96293
Alexandru Pupsa <alexandru.pupsa@bluecouchdigital.com>
2015-11-13 17:18:32 +0200

    Initial commit, includes HomePage, Login Macro and Controller

---
+++ BCDSocialNetwork/Content/scripts/main.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    $("#loginButton").on("click", function (e) {
+        e.preventDefault();
+        var loginData = {
+            username: $("#loginUsername").val(),
+            password: $("#loginPassword").val()
+        };
+        $.ajax({
+            type: "POST",
+            url: "/Umbraco/Api/Membership/Login",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: JSON.stringify(loginData)
+        }).done(function (data) {
+            if (data.success === true) {
+                $("#registerContainer").hide();
+                $("#loginContainer").hide();
+                $("#loginRegisterButtons").hide();
+                $("#loggedIn").show();
+            } else {
+                $("#loginPassword").val("");
+                $("#errorMessage").val(data.errorMessage);
+            }
+        });
+    });
+
+    $("#showLogin").on("click", function (e) {
+        e.preventDefault();
+        $("#registerContainer").hide();
+        $("#loginContainer").show();
+    });
+
+    $("#showRegister").on("click", function (e) {
+        e.preventDefault();
+        $("#registerContainer").show();
+        $("#loginContainer").hide();
+    });
+});
---
+++ BCDSocialNetwork/Controllers/MembershipController.cs
@@ -0,0 +1,29 @@
+﻿using BCDSocialNetwork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Security;
+using Umbraco.Web.WebApi;
+
+namespace BCDSocialNetwork.Controllers
+{
+    public class MembershipController : UmbracoApiController 
+    {
+        [System.Web.Http.HttpPost]
+        public object Login([FromBody] LoginModel loginData)
+        {
+            if (Membership.ValidateUser(loginData.Username, loginData.Password))
+            {
+                FormsAuthentication.SetAuthCookie(loginData.Username, false);
+                return Json(new { success = true });
+            }
+            else
+            {
+                return Json(new { success = false, errorMessage = "Invalid username or password" });
+            }
+        }
+    }
+}
