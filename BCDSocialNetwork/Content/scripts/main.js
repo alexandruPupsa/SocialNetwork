991fe4ea73036ef4be1e2b376a51083b859a9614
Alexandru Pupsa <alexandru.pupsa@bluecouchdigital.com>
2015-11-13 18:41:32 +0200

    Added Register and Logout functionality

--- BCDSocialNetwork/Content/scripts/main.js
+++ BCDSocialNetwork/Content/scripts/main.js
@@ -2,8 +2,8 @@
     $("#loginButton").on("click", function (e) {
         e.preventDefault();
         var loginData = {
-            username: $("#loginUsername").val(),
-            password: $("#loginPassword").val()
+            Username: $("#loginUsername").val(),
+            Password: $("#loginPassword").val()
         };
         $.ajax({
             type: "POST",
@@ -12,14 +12,78 @@
             dataType: "json",
             data: JSON.stringify(loginData)
         }).done(function (data) {
-            if (data.success === true) {
+            if (data.Success === true) {
                 $("#registerContainer").hide();
                 $("#loginContainer").hide();
                 $("#loginRegisterButtons").hide();
+                $("#loginUsername").val("");
+                $("#loginPassword").val("");
                 $("#loggedIn").show();
             } else {
                 $("#loginPassword").val("");
-                $("#errorMessage").val(data.errorMessage);
+                $("#errorMessage").text(data.ErrorMessage);
+                $("#errorMessage").show();
+            }
+        });
+    });
+
+    $("#registerButton").on("click", function (e) {
+        e.preventDefault();
+        var username = $("#registerUsername").val();
+        var password = $("#registerPassword").val();
+        var confirmPassword = $("#registerConfirmPassword").val();
+
+        if (password !== confirmPassword) {
+            $("#errorMessage").text("Passwords must match!");
+            $("#errorMessage").show();
+        }
+        else if(password.length < 8 || confirmPassword.length < 8) {
+            $("#errorMessage").text("Password length must be at least 8 characters!");
+            $("#errorMessage").show();
+        } else {
+            $("#errorMessage").text("");
+            $("#errorMessage").hide();
+            var registerData = {
+                Username: username,
+                Password: password
+            };
+            $.ajax({
+                type: "POST",
+                url: "/Umbraco/Api/Membership/Register",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                data: JSON.stringify(registerData)
+            }).done(function (data) {
+                if (data.Success === true) {
+                    $("#registerContainer").hide();
+                    $("#loginContainer").hide();
+                    $("#loginRegisterButtons").hide();
+                    $("#registerUsername").val("");
+                    $("#registerPassword").val("");
+                    $("#registerConfirmPassword").val("");
+                    $("#loggedIn").show();
+                } else {
+                    $("#registerPassword").val("");
+                    $("#registerConfirmPassword").val("");
+                    $("#errorMessage").text(data.Message);
+                    $("#errorMessage").show();
+                }
+            });
+        }
+    });
+
+    $("#logoutButton").on("click", function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "GET",
+            url: "/Umbraco/Api/Membership/Logout",
+        }).done(function (data) {
+            if (data.Success === true) {
+                $("#registerContainer").hide();
+                $("#loginContainer").show();
+                $("#loginRegisterButtons").show();
+                $("#loggedIn").hide();
+            } else {
             }
         });
     });
--- BCDSocialNetwork/Controllers/MembershipController.cs
+++ BCDSocialNetwork/Controllers/MembershipController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Security;
+using Umbraco.Core.Models;
 using Umbraco.Web.WebApi;
 
 namespace BCDSocialNetwork.Controllers
@@ -15,15 +16,61 @@ namespace BCDSocialNetwork.Controllers
         [System.Web.Http.HttpPost]
         public object Login([FromBody] LoginModel loginData)
         {
+            bool success = false;
+            string errorMessage = "";
+
             if (Membership.ValidateUser(loginData.Username, loginData.Password))
             {
                 FormsAuthentication.SetAuthCookie(loginData.Username, false);
-                return Json(new { success = true });
+                success = true;
             }
             else
             {
-                return Json(new { success = false, errorMessage = "Invalid username or password" });
+                success = false;
+                errorMessage = "Invalid username or password";
             }
+
+            return Json(new { Success = success, ErrorMessage = errorMessage });
         }
+
+        [System.Web.Http.HttpPost]
+        public object Register([FromBody] LoginModel registerData)
+        {
+            bool success = false;
+            string errorMessage = "";
+
+            if (!MemberExists(registerData.Username))
+            {
+                IMember newMember = ApplicationContext.Services.MemberService.CreateMember(registerData.Username, registerData.Username, registerData.Username, "Member");
+
+                try
+                {
+                    ApplicationContext.Services.MemberService.Save(newMember);
+                    ApplicationContext.Services.MemberService.SavePassword(newMember, registerData.Password);
+                    Members.Login(registerData.Username, registerData.Password);
+                    success = true;
+                }
+                catch (Exception ex)
+                {
+                    errorMessage = ex.Message;
+                    success = false;
+                }
+            }
+
+            return Json(new { Success = success, ErrorMessage = errorMessage });
+        }
+
+        [System.Web.Http.HttpGet]
+        public object Logout()
+        {
+            FormsAuthentication.SignOut();
+            return Json(new { Success = true });
+        }
+
+        private bool MemberExists(string username)
+        {
+            return (ApplicationContext.Services.MemberService.GetByUsername(username) != null);
+        }
+
     }
 }
